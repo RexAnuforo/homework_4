8c987e6e3fd6f7e93d5bad0924ce08125203dbaf
Rex A <rexcanuforo@yahoo.com>
2020-04-10 23:14:31 -0400

    rough draft

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const startButton=document.getElementById('startBtn')
+const questionContainerElement=document.getElementById('questContainer')
+
+startButton.addEventListener('click',startGame)
+
+function startGame(){
+    console.log('Activated')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+}
+
+function updateLocalStorage(){ 
+    var scoreSrt=JSON.stringify(string);
+
+
+}
+
+var 
---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+// define our questions
+
+//have an idea of our HTML layout 
+
+//<h2> countdown timer 
+
+//<ul><li> questions
+
+//create a button with event llistener to start the quiz.
+
+//load in timer and question 
+
+//if and else for questions correction
+
+//indicate weather or not questions was correct
+
+//if incorrect subtract time (10seconds)
+
+//if correct. increment score ++
+
+//loud up question 2 (repeat until question 5)
+
+// loud up game over indication & score 
+
+//local store to save info
+
+var obj={quest:["Question 1","Question 2","Question 3","Question 4","Question 5","Question 6"],Answers:["ans 1","ans 2","ans 3","ans 4","ans  5","ans 6"],fakeAnswers["fans 1","fans 2","fans 3","fans 4","fans  5","fans 6""fans 7","fans 8","fans 9","fans 10","fans  11","fans 12""fans 13","fans 13","fans 13","fans 14","fans  15","fans 6""ans 1","ans 2","ans 3","ans 4","ans  5","ans 6"]
+}
+
+
+
+function setNextQuestion(){
+
+}
+function selectAnswer(){
+
+}
+
+function onRemoveClick(event){
+    event.preventDefault();
+    if event.target.matches("startBtn") 
+    
+}
